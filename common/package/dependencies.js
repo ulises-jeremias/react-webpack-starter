6e00f9797d2e55d862b5425db024aa09a2e046ab
ulises-jeremias <ulisescf.24@gmail.com>
2020-09-22 00:48:03 -0300

    Added base

---
+++ common/package/dependencies.js
@@ -0,0 +1,11 @@
+module.exports = [
+  "i18next",
+  "i18next-browser-languagedetector",
+  "i18next-http-backend",
+  "moment-timezone",
+  "prop-types",
+  "react",
+  "react-dom",
+  "react-i18next",
+  "react-router-dom",
+]
